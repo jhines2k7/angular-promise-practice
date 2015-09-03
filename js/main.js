db935cc5a1df31781336143148b2efab52257a62
James Hines <jameshines10@gmail.com>
2015-09-03 12:05:38 -0400

    Experiments with some different ways of handling errors with the  service

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 (function(){
 	angular.module('main', [])
 
-	.service('SimpleService', function(){})
+	.service('SimpleService', function($http){
+		this.getWeather = function(){
+			return $http.get('http://api.openweathermap.org/data/2.5/weather?q=narnia')
+				.then(function(response){
+					return response;
+				})
+				.catch(function(err){
+					return err;
+				});
+		}
+	})
 
-	.controller('SimpleController', function($scope){
+	.controller('SimpleController', function($scope, SimpleService, $log){
+		SimpleService.getWeather()
+			.then(function(response){
+				$scope.weatherData = response.data;
+			})
+			.catch(function(err){
+				$log.error(err);
+			});
+
 		$scope.data = {
 			foo: 'This is foo',
 			bar: 'This is bar',
